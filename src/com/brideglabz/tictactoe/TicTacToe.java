0c2b3cc831027fb42b080cb878b8980f81298ec9
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-06 08:21:42 +0530

    REFACTORED: method to create board

--- src/com/brideglabz/tictactoe/TicTacToe.java
+++ src/com/brideglabz/tictactoe/TicTacToe.java
@@ -5,7 +5,7 @@ public class TicTacToe {
 	static char[] board = new char[10];
 	
 	static void createBoard(){
-		for(int index = 0 ; index<10 ; index++) {
+		for(int index = 1 ; index<10 ; index++) {
 			board[index] = ' ';
 		}
 	}
