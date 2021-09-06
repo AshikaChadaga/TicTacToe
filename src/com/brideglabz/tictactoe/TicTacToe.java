79c716be1106185d16fa24ddd8cfcbf05f41c334
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-06 08:42:22 +0530

    REFACTORED: method to get player's choice

--- src/com/brideglabz/tictactoe/TicTacToe.java
+++ src/com/brideglabz/tictactoe/TicTacToe.java
@@ -1,8 +1,12 @@
 package com.brideglabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 	
 	static char[] board = new char[10];
+	static char player,computer;
+	public static Scanner scanner = new Scanner(System.in);
 	
 	static void createBoard(){
 		for(int index = 1 ; index<10 ; index++) {
@@ -10,10 +14,22 @@ public class TicTacToe {
 		}
 	}
 	
+	static void getPlayerChoice() {
+		 System.out.print("select x or o : ");
+		 player = scanner.next().charAt(0);
+		 if (player == 'x')
+			 computer='o';
+		 else
+			 computer ='x';
+		 System.out.println("You have selected : " +player);
+		 System.out.println("Computer's choice is : " +computer);
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
 		createBoard();
+		getPlayerChoice();
 		
 
 	}
