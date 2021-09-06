e396752a7015275d6b21de3dc1bb6b686058cbb8
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-06 08:18:22 +0530

    REFACTORED: method to create board

--- src/com/brideglabz/tictactoe/TicTacToe.java
+++ src/com/brideglabz/tictactoe/TicTacToe.java
@@ -1,10 +1,20 @@
 package com.brideglabz.tictactoe;
 
 public class TicTacToe {
-
+	
+	static char[] board = new char[10];
+	
+	static void createBoard(){
+		for(int index = 0 ; index<10 ; index++) {
+			board[index] = ' ';
+		}
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Tic Tac Toe");
+		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+		createBoard();
+		
 
 	}
 
